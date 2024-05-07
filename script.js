780392ced1ff11dad66db8bcdede5858f3c80eaf
Bustavo-coder <151396094+Bustavo-coder@users.noreply.github.com>
2024-05-07 23:42:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const ptoggle = document.getElementById("menu-toggle")
+const menu = document.getElementById("navigation")
+const navigate = document.getElementById("navigate")
+const slides = document.getElementsByClassName("slide")
+const active = () =>{
+    menu.classList.toggle("active")
+    // navigate.classList.toggle("active")
+    console.log("e don click")
+}
+ptoggle.addEventListener("click", active)
+
+let slideIndex = 0
+
+const hide = () => {
+    for(var i = 0; i < slideIndex; i++){
+        slides[i].style.display="none"
+    }
+}
+
+const showSlides = () => {
+    hide ()
+
+    slideIndex++
+    if(slideIndex > slides.length){
+        slideIndex = 1
+    }
+   
+    slides[slideIndex -1].style.display="block"
+    setTimeout(showSlides , 1000)
+}
+showSlides ()
+
+// var adeyemi = alert("You Are Welcome To Finder? Are You Competent Then Let Find You A Job")
+
+
+
+
+
